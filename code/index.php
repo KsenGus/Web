720ea96d9548869f06b87a8245065a3796a13c7c
KsenGus <99027383+KsenGus@users.noreply.github.com>
2022-02-21 13:51:57 +0300

    task 1

--- code/index.php
+++ code/index.php
@@ -1,2 +1,11 @@
 <?php
-echo phpinfo();
+    /* Imagine a lot of code here */
+
+    //1.Доступ по ссылке
+    echo "Task 1" . "<br>";
+    $very_bad_unclear_name = "15 chicken wings";
+    // Write your code here:
+    $order = &$very_bad_unclear_name;
+    $order .= " and coffee";
+    //Don't change the line below
+    echo "\nYour order is: $very_bad_unclear_name.";
